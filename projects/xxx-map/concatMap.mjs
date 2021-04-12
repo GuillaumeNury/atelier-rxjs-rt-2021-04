b8fb9422b24d0971cb2219e884c4861ef079ad97
Guillaume Nury <nury.guillaume@gmail.com>
2021-04-12 15:53:56 +0200

    feat: add xxx-map examples

---
+++ projects/xxx-map/concatMap.mjs
@@ -0,0 +1,22 @@
+import { of, BehaviorSubject } from 'rxjs';
+import { concatMap, delay } from 'rxjs/operators/index.js';
+
+const search$ = new BehaviorSubject('d');
+const origin = Date.now();
+const roundedNow = () => Math.floor((Date.now() - origin) / 100) * 100;
+
+const httpClient = {
+  get: (search) => of({ search }).pipe(delay(500)),
+};
+
+const users$ = search$.pipe(
+  concatMap(search => httpClient.get(search)),
+);
+
+users$.subscribe(users => console.log(`Time: ${roundedNow()}\tResult: ${users.search}`));
+
+setTimeout(() => search$.next('da'), 200);
+setTimeout(() => search$.next('dan'), 400);
+setTimeout(() => search$.next('dani'), 600);
+setTimeout(() => search$.next('danie'), 800);
+setTimeout(() => search$.next('daniel'), 1000);
